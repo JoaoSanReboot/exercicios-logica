319ac1f329b0b82e0c60276cb2738bcaeb665ddb
JoaopedroSantana1 <joaopedrosantanarosa@gmail.com>
2024-01-10 15:25:23 -0300

    New exercice.exe

---
+++ Algoritimos/ex-8.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+int main(void){
+    
+    //Para acentuação brasileira
+    setlocale(LC_ALL, "Portuguese_Brazil");
+
+    char escolha;
+    float celsius, fahrenheit;
+
+    printf("Qual escala de temperatura você vai usar (C/F)? \n");
+        scanf("%c", &escolha);
+
+    if(escolha == 'f' || escolha == 'F'){
+
+        printf("Digite a temperatura em Fahrenheit: \n");
+            scanf("%f", &fahrenheit);
+                celsius = 5.0 / 9.0 * (fahrenheit - 32.0);
+            printf("Temperatura equivalente em Celsius: %.2f", celsius);
+    }
+    if(escolha == 'c' || escolha == 'C'){
+
+        printf("Digite a temperatura em Celsius: \n");
+            scanf("%f", &celsius);
+                fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
+            printf("Temperatura equivalente em Fahrenheit: %.2f", fahrenheit);
+    }    
+}
