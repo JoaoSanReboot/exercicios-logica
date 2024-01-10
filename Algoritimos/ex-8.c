0608733edbb2dc9e210a112d8ec393c5f46daf47
JoaopedroSantana1 <joaopedrosantanarosa@gmail.com>
2024-01-10 15:34:14 -0300

    Update ex.exe

--- Algoritimos/ex-8.c
+++ Algoritimos/ex-8.c
@@ -7,12 +7,15 @@ int main(void){
     //Para acentuação brasileira
     setlocale(LC_ALL, "Portuguese_Brazil");
 
+    //Declaração de variáveis
     char escolha;
     float celsius, fahrenheit;
 
+    //Lê a escolha informada
     printf("Qual escala de temperatura você vai usar (C/F)? \n");
         scanf("%c", &escolha);
 
+    //Faz o calcúlo de reversão para celcius
     if(escolha == 'f' || escolha == 'F'){
 
         printf("Digite a temperatura em Fahrenheit: \n");
@@ -20,6 +23,7 @@ int main(void){
                 celsius = 5.0 / 9.0 * (fahrenheit - 32.0);
             printf("Temperatura equivalente em Celsius: %.2f", celsius);
     }
+    //Faz o calcúlo de reversão para fahrenheit
     if(escolha == 'c' || escolha == 'C'){
 
         printf("Digite a temperatura em Celsius: \n");
