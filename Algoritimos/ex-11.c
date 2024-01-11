745dba15f3d9765a8a4687bf7beac50d1ded3c61
JoaopedroSantana1 <joaopedrosantanarosa@gmail.com>
2024-01-11 12:56:26 -0300

    Update ex-11.exe

--- Algoritimos/ex-11.c
+++ Algoritimos/ex-11.c
@@ -6,6 +6,7 @@ void calcularAumento(float salario, float *novoSalario, float *aumento, float *p
     // Para acentuação brasileira
     setlocale(LC_ALL, "Portuguese_Brazil");
 
+    //If's para ler o salário e fazer o aumento com a porcentagem
     if (salario <= 1000.00 && salario > 0) {
         *porcentagem = 20;
     }
@@ -19,29 +20,35 @@ void calcularAumento(float salario, float *novoSalario, float *aumento, float *p
         *porcentagem = 5;
     }
 
+    //Calcúlo de aumento de salario usando a porcentagem
     *aumento = salario * (*porcentagem / 100);
     *novoSalario = salario + *aumento;
 }
 
 int main() {
+
+    //Declaração de variáveis
     float salario, novoSalario, aumento, porcentagem;
     
     // Para acentuação brasileira
     setlocale(LC_ALL, "Portuguese_Brazil");
 
+    //Lê o salário do funcionário
     printf("Digite o salário do funcionário: \n");
-    scanf("%f", &salario);
+        scanf("%f", &salario);
 
+    //Caso o usuario não informe corretamente
     if (salario <= 0) {
+
         printf("\n\033[0;31mInforme um salário correto.\n");
-        return 1;  // Código de erro
     }
 
+    //Puxa a função para calcúlar a porcentagem
     calcularAumento(salario, &novoSalario, &aumento, &porcentagem);
 
+    //Impri os dados dos calcúlos
     printf("\nNovo salário: R$%.2f\n", novoSalario);
     printf("Aumento de salário: R$%.2f\n", aumento);
     printf("Porcentagem: %.0f%%\n", porcentagem);
 
-    return 0;
 }
