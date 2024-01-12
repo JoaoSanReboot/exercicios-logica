e3160996e764d2186586878f748c5ed63d54bfe0
JoaopedroSantana1 <joaopedrosantanarosa@gmail.com>
2024-01-12 17:33:25 -0300

    Update pastes.exe

---
+++ Algoritimos/Exercicios/Pdf-4/ex-1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+int main(void){
+    
+    //Para acentuação brasileira
+    setlocale(LC_ALL, "Portuguese_Brazil");
+
+    //Declaração de variáveis
+    float nota1, nota2, media;
+
+    printf("Digite a nota do primeiro semestre: \n");
+    scanf("%f", &nota1);
+
+    printf("Digite a nota do segundo semestre: \n");
+    scanf("%f", &nota2);
+
+    //Calcúlo de média
+    media = nota1 + nota2;
+
+    //If para dicernir a média
+    if(media >= 60.0){
+
+        printf("\033[1;32m Parabéns você foi aprovado sua média total foi: %.1f", media);
+    }
+    else{
+        printf("\033[1;31m Sinto muito sua nota foi abaixo da média você foi reprovado! \n");
+        printf("\033[1;31m Sua média total é: %.1f", media);
+    }
+
+    return 0;
+
+} 
